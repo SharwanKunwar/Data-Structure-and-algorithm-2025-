0c305ab54a2ea6aafeb99f89a7e6807205d51524
Sharwan jung kunwar <sharwankunwar07@gmail.com>
2025-02-04 13:34:25 +0545

    added something code06

--- Demo_code/code06.java
+++ Demo_code/code06.java
@@ -4,42 +4,34 @@ package Demo_code;
 //Purpose: Print the given pattern.
 
 /* Ex: - for: 4
-                      *
-                    * * *
-                  * * * * *
-                * * * * * * *
+                            *
+                          * * *
+                        * * * * *
+                      * * * * * * *
  */
 
 import java.util.Scanner;
 
-public class method_another {
+public class code06 {
     public static void main(String[] args)
     {
         //creating a scanner object to take input from the user.
         Scanner scan = new Scanner(System.in);
 
-
-        System.out.print("Enter your size : ");
+        System.out.print("Enter your Num : ");
         int num = scan.nextInt();
-
-        int space = num-1;
-        int pattern =1;
 
         //logic
         for(int i=1; i<=num; i++){
-            for(int j=1; j<=space; j++){
+            for(int s=1; s<=i; s++){
                 System.out.print("  ");
             }
-            for(int k=1; k<=pattern; k++){
+            for(int j=2; j<=(2*num-i)-i; j++){
                 System.out.print("* ");
             }
-            space--;
-            pattern+=2;
             System.out.println();
+
         }
-
-
-
 
     }
 }
